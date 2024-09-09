0dd7c568b8c30127404a99d190afc0d47efe3838
Tomasz Tkocz <tkoczt@gmail.com>
2024-09-09 19:56:07 +0200

    Initial commit

---
+++ HouseManagmentMobile/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HouseManagmentMobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HouseManagmentMobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace HouseManagmentMobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
